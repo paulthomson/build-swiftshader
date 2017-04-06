6c7beaa74b8d5601c153ffeeb9beb8847d32f0a9
Paul Thomson <paul.thomson11@imperial.ac.uk>
2017-04-06 20:45:29 +0100

    Initial commit.

---
+++ 3-release-appveyor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+set -e
+set -u
+
+shopt -s extglob
+shopt -s nullglob
+
+cd swiftshader
+
+CMAKE_BUILD_TYPE="${Configuration}"
+# swiftshader does not install anything, so use build directory + ${Configuration}.
+INSTALL_DIR="${PROFILE}-${Platform}-${CMAKE_BUILD_TYPE}-build/${Configuration}"
+COMMIT_ID="${APPVEYOR_REPO_COMMIT}"
+
+pushd "${INSTALL_DIR}"
+7z a "../../${INSTALL_DIR}.zip" !(*.lib)
+popd
+
+github-release \
+  paulthomson/build-swiftshader \
+  "v-${COMMIT_ID}" \
+  "${COMMIT_ID}" \
+  "$(echo -e "Automated build.\n$(git log --graph -n 3 --abbrev-commit --pretty='format:%h - %s <%an>')")" \
+  "${INSTALL_DIR}.zip"
+
