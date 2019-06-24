07f9c96ac35a6f9ed114cb2b5b5068b2e60a2875
Paul Thomson <paulthomson@google.com>
2019-06-24 15:11:28 +0100

    Allow warnings. Build everything on Windows.

--- 2-build-appveyor.sh
+++ 2-build-appveyor.sh
@@ -10,9 +10,7 @@ BUILD_DIR="${INSTALL_DIR}-build"
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 cmake -G "${CMAKE_GENERATOR}" .. "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" -DCMAKE_OSX_ARCHITECTURES=x86_64 ${CMAKE_OPTIONS}
-cmake --build . --config "${CMAKE_BUILD_TYPE}" --target libEGL
-cmake --build . --config "${CMAKE_BUILD_TYPE}" --target libGLESv2
-cmake --build . --config "${CMAKE_BUILD_TYPE}" --target libGLES_CM
+cmake --build . --config "${CMAKE_BUILD_TYPE}"
 cmake "-DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}" "-DBUILD_TYPE=${CMAKE_BUILD_TYPE}" -P cmake_install.cmake
 cd ..
 
